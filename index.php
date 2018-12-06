8c67bf85db566a3bc4794f2119ab0c1ff9ac10b5
littleblackman <sandyrazafitrimo@gmail.com>
2018-12-06 19:40:15 +0100

    update

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 // configuration
 include_once('_config.php');
 
-
-
-
 if($_GET['action'] == 'home')
 {
     include_once(CONTROLLER.'home.php');
@@ -15,4 +12,3 @@ if($_GET['action'] == 'new')
 {
     include_once(CONTROLLER.'new.php');
 }
-
